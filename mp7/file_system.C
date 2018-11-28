5477a369405660a2c485db52eeebe4d534f0621e
Sandeep Gottimukkala <sgottimukkal@tamu.edu>
2018-11-28 01:49:27 -0600

    MP7: Create file

--- mp7/file_system.C
+++ mp7/file_system.C
@@ -52,8 +52,8 @@ bool FileSystem::Format(SimpleDisk * _disk, unsigned int _size) {
     _disk->read(0, buf);
     memcpy(buf, &_size, 4);
 
-    unsigned int number_of_files = 0;
-    memcpy(buf+4, &number_of_files, 4);
+    unsigned int num_created = 0;
+    memcpy(buf+4, &num_created, 4);
     _disk->write(0, buf);
    // then we read the free_blocks_block and reset it such that all blocks (except 1 and 2) are 0
     
@@ -63,7 +63,7 @@ bool FileSystem::Format(SimpleDisk * _disk, unsigned int _size) {
 
     // freeing up all disk blocks
     for (int i=4; i < 512; i+=4) {
-      unsigned int val = 0x0000;
+      unsigned int val = 0;
       memcpy(buf+i, &val, 4);
     }
     _disk->write(1, buf);
@@ -73,13 +73,23 @@ File * FileSystem::LookupFile(int _file_id) {
     Console::puts("looking up file\n");
     unsigned char inode[512];
     disk->read(inode_block_num, inode);
-    unsigned int num_files = 0;
-    memcpy(&num_files, inode+4, 4);
+    unsigned int num_created = 0;
+    memcpy(&num_created, inode+4, 4);
     
-    for(int i=0; i < num_files; i+=24) {
+    // iterating all created files
+    for(int i=0; i < num_created; i+=16) {
         int id = 0;
         memcpy(&id, inode+8+i, 4);
+
+        // id matched
         if (_file_id == id) {
+            unsigned int is_deleted = 0;
+            memcpy(&is_deleted, inode+8+i+12, 4);
+
+            // ensure this is not deleted
+            if (is_deleted == 1) {
+               continue;
+            }
             return new File();
         }
     }
@@ -88,7 +98,42 @@ File * FileSystem::LookupFile(int _file_id) {
 
 bool FileSystem::CreateFile(int _file_id) {
     Console::puts("creating file\n");
-    assert(false);
+    unsigned char inode[512];
+    disk->read(inode_block_num, inode);
+    unsigned int num_created = 0;
+    memcpy(&num_created, inode+4, 4);
+
+    unsigned char free_blocks[512];
+    unsigned int block_num = -1;
+    disk->read(free_block_num, free_blocks);
+
+    // finding a free disk block
+    for(int i=2; i+8 < 512; i+=8) {
+       unsigned int val = 0;
+       memcpy(&val, free_blocks+i, 4);
+       if ((val & 0x8000 == 0)) {
+          val = 0x8000;
+          memcpy(free_blocks+i, &val, 4);
+          block_num = i*8;
+       }
+    }
+    if (block_num == -1) {
+        return false;
+    }
+
+    // writing to the inode block
+    int file_size = 0;
+    int is_deleted = 0;
+    memcpy(inode+8+num_created, &_file_id, 4);
+    memcpy(inode+8+num_created + 4, &file_size, 4);
+    memcpy(inode+8+num_created + 8, &block_num, 4);
+    memcpy(inode+8+num_created + 12, &is_deleted, 4);
+
+
+    // writing blocks back to disk
+    disk->write(free_block_num, free_blocks);
+    disk->write(inode_block_num, inode);
+    return true;
 }
 
 bool FileSystem::DeleteFile(int _file_id) {
